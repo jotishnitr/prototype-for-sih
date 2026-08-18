09f7beeef8526e622306a3893868e2ba001a0d7d
aryanbiswal24 <aryanbiswal2486@gmail.com>
2026-08-18 22:19:45 +0530

    error fixing

--- team-altiora/src/main.jsx
+++ team-altiora/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './layout.jsx'
-import Home from './components/Home/home.jsx'
-import About from './components/About/about.jsx'
-import Contact from './components/Contact/contact.jsx'
-import User from './components/User/user.jsx'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
